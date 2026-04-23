9b3bbed70004efdff5e2de53f4191014c30b8706
maayan459 <maayanconley5@gmail.com>
2026-04-23 10:44:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ st.write("Omer's website is not that succssesful as Maayan's website")
 
 
 name=st.text_input("do you think that Maayan is the queen?")
-st.write(name)
+
+st.write("Omer is the smartest and the most stupid kid ever")
+
